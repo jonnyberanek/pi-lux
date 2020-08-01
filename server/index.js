3987ab2f7c7590df6eb8f747b917d0223a033579
Jonny Beranek <jonnyberanek@gmail.com>
2020-08-01 15:38:38 -0400

    Added babel config

--- server/index.js
+++ server/index.js
@@ -1,17 +1,15 @@
-const express = require('express')
+import express from 'express'
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   try{
-    	
-	
-	
 
   } catch(e){
 
   }
-
+  res.send('hello')
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
